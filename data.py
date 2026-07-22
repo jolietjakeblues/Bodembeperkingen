a4d3490844bc717a58fc2e0ee423e894ec0c038e
jolietjakeblues <jolietjakeblues64@gmail.com>
2026-07-22 13:31:44 +0200

    add: data.py

---
+++ data.py
@@ -0,0 +1,63 @@
+BODEM_TOTAAL = 2436
+BODEM_SMAL = 222
+BODEM_BREED = 393
+
+BASELINE_TOTAAL = 63097
+BASELINE_SMAL = 2279
+BASELINE_BREED = 6906
+
+SMAL_MATCHES = {
+    "Fabriek-familie": {
+        "Machinefabriek": 22, "Fabrieksgebouw": 16, "Fabrieksschoorsteen": 7,
+        "Fabriekswoning": 4, "Gasfabriek": 4, "Fabriekshal": 3, "Meelfabriek": 2,
+        "Zuivelfabriek": 2, "Aardewerkfabriek": 2, "Cacaofabriek": 2,
+        "Kaasfabriek": 1, "Leerfabriek": 1, "Suikerfabriek": 1, "Tapijtfabriek": 1,
+        "Metaalfabriek": 1, "Verffabriek": 1, "Kunstzijdefabriek": 1, "Broodfabriek": 1,
+        "Suikerwerkfabriek": 1, "Bedrijfs-,fabriekswoning": 1,
+    },
+    "Molen / gemaal": {
+        "Molen": 22, "Gemaal(M3)": 2, "Rioolgemaal": 1, "Gemaal(M)": 1,
+        "Gemaal (M)": 1, "Industrie- en poldermolen": 1, "Korenmolen": 1,
+    },
+    "Werf / scheepvaart / spoor": {
+        "Waterweg, werf en haven": 11, "Werfgebouw": 7, "Spoorbrug": 3,
+        "Scheepswerf": 2, "Spoorbaan": 2, "Spoorwegstation": 1, "Scheepvaartgebouw": 1,
+    },
+    "Industrie / nijverheid (generiek)": {
+        "Industrie": 23, "Nijverheid": 12,
+    },
+    "Overig (ambacht / techniek)": {
+        "Mijnwerkerswoningen": 13, "Ketelhuis": 10, "Elektriciteitscentrale": 6,
+        "Maalderij": 5, "Transformatorgebouw": 4, "Bierbrouwerij": 4,
+        "Machinegebouw(M)": 2, "Smederij": 2, "Leerlooierij": 2,
+    },
+}
+
+BREED_EXTRA_MATCHES = {
+    "Werk-woonhuis": 96, "Pakhuis": 28, "Opslag": 28, "Magazijn": 13,
+}
+
+GEMIST = {
+    "Branderij": 13, "Loods": 7, "Silo": 3, "Pompgebouw(M05)": 3,
+    "Remise (B)": 3, "Locomotievenremise": 2, "Perrongebouw": 2,
+    "Zoutziederij": 3, "Drukkerij": 5, "IJzergieterij": 2, "Zeepziederij": 1,
+    "Koperslagerij": 1, "Olieslagerij": 1, "Pellerij": 1, "Schoenmakerij": 1,
+    "Goederenloods": 1, "Wasserij": 1, "Vernisstokerij": 1, "Zuiveringshuis": 2,
+}
+
+BASELINE_TOP = {
+    "Woonhuis(K)": 16110, "Woonhuis": 10432, "Boerderij(M)": 5483, "Boerderij (M)": 5483,
+    "Werk-woonhuis": 3483, "Kerk": 2306, "Archeologie (N1)": 1461, "Tuin, park en plantsoen": 1237,
+    "Molen": 1126, "Boerderij(M1)": 1009, "Bijgebouwen kastelen enz.": 564,
+    "Historische aanleg": 533, "Pakhuis": 526, "Opslag": 480, "Kasteel, buitenplaats": 460,
+    "Landhuis": 450, "Fort, vesting en -onderdelen": 436, "Schoolgebouw": 355,
+    "Industrie": 168, "Nijverheid": 71,
+}
+
+BODEM_TOP = {
+    "Woonhuis(K)": 428, "Woonhuis": 387, "Werk-woonhuis": 96, "Bijgebouwen": 67,
+    "Legeringsgebouw": 55, "Klooster, kloosteronderdl": 22, "Kerk": 48,
+    "Fort, vesting en -onderdelen": 34, "Boerderij(M)": 40, "Boerderij (M)": 40,
+    "Pakhuis": 28, "Opslag": 28, "Machinefabriek": 22, "Molen": 22,
+    "Industrie": 23, "Omwalling": 30, "Mijnwerkerswoningen": 13,
+}
